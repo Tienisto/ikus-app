ec272f69fca79accd1b7e0477c1a16c1882b1423
Tien Do Nam <dev.tien.donam@gmail.com>
2020-09-29 02:48:27 +0200

    fix: handle no place in event cards

--- lib/components/cards/event_card.dart
+++ lib/components/cards/event_card.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:ikus_app/components/cards/ovgu_card.dart';
 import 'package:ikus_app/components/icon_text.dart';
+import 'package:ikus_app/i18n/strings.g.dart';
 import 'package:ikus_app/model/event.dart';
 import 'package:ikus_app/utility/callbacks.dart';
 import 'package:ikus_app/utility/ui.dart';
@@ -34,7 +35,7 @@ class EventCard extends StatelessWidget {
               IconText(
                 size: 16,
                 icon: Icons.place,
-                text: event.place,
+                text: event.place ?? t.components.eventCard.noPlace,
               )
             ],
           ),
