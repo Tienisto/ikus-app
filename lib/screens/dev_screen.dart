9f3050a95e8153e124775bdf5e9ff5ad0514bc4c
Tien Do Nam <dev.tien.donam@gmail.com>
2020-10-03 16:00:13 +0200

    feat: add dev button to start tutorial

--- lib/screens/dev_screen.dart
+++ lib/screens/dev_screen.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:ikus_app/components/buttons/ovgu_button.dart';
 import 'package:ikus_app/components/main_list_view.dart';
 import 'package:ikus_app/components/ovgu_switch.dart';
 import 'package:ikus_app/components/settings_item.dart';
 import 'package:ikus_app/i18n/strings.g.dart';
+import 'package:ikus_app/screens/main_screen.dart';
 import 'package:ikus_app/service/settings_service.dart';
+import 'package:ikus_app/utility/globals.dart';
 import 'package:ikus_app/utility/ui.dart';
 
 class DevScreen extends StatefulWidget {
@@ -36,6 +39,17 @@ class _DevScreenState extends State<DevScreen> {
                 });
               }
             ),
+          ),
+          SizedBox(height: 20),
+          SettingsItem(
+              left: 'Show Tutorial',
+              right: OvguButton(
+                type: OvguButtonType.ICON_WIDE,
+                callback: () {
+                  setScreen(context, () => MainScreen(tutorial: true));
+                },
+                child: Icon(Icons.restore, color: Colors.white),
+              )
           )
         ],
       ),
