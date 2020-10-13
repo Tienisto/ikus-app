2480ffbd19f31d02420059ee8cd3654cc0544074
Tien Do Nam <dev.tien.donam@gmail.com>
2020-10-13 18:17:36 +0200

    fix: GenericConfirmPopup

--- lib/components/popups/generic_confirm_popup.dart
+++ lib/components/popups/generic_confirm_popup.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:ikus_app/components/buttons/ovgu_button.dart';
-import 'package:ikus_app/i18n/strings.g.dart';
 import 'package:ikus_app/utility/ui.dart';
 
 class GenericConfirmPopup extends StatelessWidget {
@@ -27,12 +25,7 @@ class GenericConfirmPopup extends StatelessWidget {
                   Center(child: Text(info, style: TextStyle(fontSize: 16), textAlign: TextAlign.center)),
                   if (buttons.length == 1)
                     Center(
-                      child: OvguButton(
-                        callback: () {
-                          Navigator.pop(context);
-                        },
-                        child: Text(t.popups.eventPastPopup.ok, style: TextStyle(color: Colors.white)),
-                      ),
+                      child: buttons.first,
                     ),
                   if (buttons.length > 1)
                     Row(
