689ce4931d0b7fcc53ae5a21cffde84ed258ebb8
Tien Do Nam <dev.tien.donam@gmail.com>
2020-11-25 15:37:48 +0100

    feat: include workmanager again

--- lib/service/background_service.dart
+++ lib/service/background_service.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:background_fetch/background_fetch.dart';
 import 'package:ikus_app/service/notification_service.dart';
+import 'package:workmanager/workmanager.dart';
 
 /// executes code in background
 class BackgroundService {
@@ -8,8 +11,18 @@ class BackgroundService {
   static BackgroundService get instance => _instance;
 
   void init() {
-    final config = BackgroundFetchConfig(minimumFetchInterval: 15);
-    BackgroundFetch.configure(config, backgroundTaskDispatcher);
+    if (Platform.isAndroid) {
+      Workmanager.initialize(workmanagerWrapper);
+      Workmanager.registerPeriodicTask(
+          "1",
+          "fetchTask",
+          frequency: Duration(minutes: 15), // at least 15min
+          existingWorkPolicy: ExistingWorkPolicy.replace
+      );
+    } else {
+      final config = BackgroundFetchConfig(minimumFetchInterval: 15, startOnBoot: true, enableHeadless: true, stopOnTerminate: false);
+      BackgroundFetch.configure(config, backgroundTaskDispatcher);
+    }
   }
 }
 
@@ -17,5 +30,14 @@ void backgroundTaskDispatcher(String taskId) {
   print('Running background task... ($taskId)');
   NotificationService notificationService = NotificationService.createInstance();
   notificationService.showTest();
-  BackgroundFetch.finish(taskId);
+
+  if (!Platform.isAndroid)
+    BackgroundFetch.finish(taskId);
+}
+
+void workmanagerWrapper() {
+  Workmanager.executeTask((String task, Map<String, dynamic> inputData) {
+    backgroundTaskDispatcher(task);
+    return Future.value(true);
+  });
 }
